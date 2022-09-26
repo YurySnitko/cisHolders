34b7ff4716cfbd4ec290154acf6ce1ac07bc68ed
Yury Snitko <yury.snitko@gmail.com>
2022-09-26 14:25:07 +0300

    installed package

--- src/scenes/Map/index.tsx
+++ src/scenes/Map/index.tsx
@@ -1,12 +1,23 @@
 import React, { FC } from 'react';
 import { Text } from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { styles } from './styles';
 
 export const MapScreen: FC = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Map screen</Text>
+      {/* <Text>Map screen</Text> */}
+      <MapView
+        style={styles.map}
+        provider={PROVIDER_GOOGLE}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
     </SafeAreaView>
   );
 };
--- src/scenes/Map/styles.ts
+++ src/scenes/Map/styles.ts
@@ -2,7 +2,13 @@ import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
   container: {
+    ...StyleSheet.absoluteFillObject,
     backgroundColor: '#fff',
     flex: 1,
+    width: 400,
+    height: 400,
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
   },
 });
